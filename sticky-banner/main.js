7d923ad079dc5e3458a84402b8ebf659c18a36d0
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2017-12-13 11:58:13 -0500

    added max width to responsive styles and making final changes

--- sticky-banner/main.js
+++ sticky-banner/main.js
@@ -1,13 +1,1 @@
-var target = document.querySelector('#the-target');
-var topOfTarget = target.offsetTop;
 
-
-function stickyBan() {
-  if(window.scrollY >= topOfTarget){
-    document.getElementById('the-target').classList.add('fixed-target');
-  } else {
-    document.getElementById('the-target').classList.remove('fixed-target');
-  }
-}
-
-window.addEventListener('scroll', stickyBan);
